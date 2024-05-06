331403c6b3066687c0954889b880f0425d9c917e
AMXX1679 <88097562+AMXX1679@users.noreply.github.com>
2024-05-06 12:04:54 +0200

    Update setup.ino
    edit setup.ino

--- setup.ino
+++ setup.ino
@@ -1,3 +1,4 @@
+
 // *** MAIN SETTINGS ***
 // Replace this block with correct template settings.
 // You can find it for every template here:
@@ -5,13 +6,13 @@
 //   https://blynk.cloud/dashboard/templates
 
 #define BLYNK_TEMPLATE_ID "YOUR_TEMPLATE_ID"
-#define BLYNK_TEMPLATE_NAME "Simple Weather Station"
+#define BLYNK_TEMPLATE_NAME "YOUR_TEMPLATE_NAME"
 #define BLYNK_AUTH_TOKEN "YOUR_AUTH_TOKEN"
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-#define WIFI_SSID "Raum 306"
-#define WIFI_PASS "schokimagessen306"
+#define WIFI_SSID "YOUR_SSID"
+#define WIFI_PASS "YOUR_WIFI_PASS"
 #define BLYNK_PRINT Serial  // Comment this out to disable serial monitor prints
 
 // Adding the required libraries
@@ -19,39 +20,27 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h> //You need to add it by searching "Adafruit unified sensor" in libraries and install it
+#include <Adafruit_BMP280.h> //You need to add it by searching "Adafruit BMP280" in libraries and install it
 #include <BlynkSimpleEsp32.h> //You need to add it by searching "Blynk" in libraries and install it
 #include <DHT.h> //You need to add it by searching "DHT sensor library" in libraries and install it
-#include "MQ135.h"
-#include <U8g2lib.h>
-
+#include <Adafruit_GFX.h>
 
 #define RZERO 225000
 
 // DHT sensor settings and configuration
 #define DHT_BLYNK_VPIN_TEMPERATURE V0 //Virtual pin on Blynk side
 #define DHT_BLYNK_VPIN_HUMIDITY V1 //Virtual pin on Blynk side
-#define MQ135_BLYNK_VPIN_PPM V3
+#define MHZ_BLYNK_VPIN_PPM V3
 
 #define DHTPIN 26
 #define DHTTYPE DHT11 
 DHT dht(DHTPIN, DHTTYPE);
 
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
-
 #define ALTITUDE_0 (1013.25)
-
-#include "MQ135.h"
-#define RZERO 225000
-
 
 #define PIN_RED 4
 #define PIN_GREEN 5
 #define PIN_BLUE 2
-
-MQ135 gasSensor = MQ135(32);
-int val;
-int sensorPin = 32;
-int sensorValue = 0;
 
 // This function creates the timer object. It's part of Blynk library 
 BlynkTimer timer; 
@@ -66,7 +55,12 @@ float DHT_TEMPERATURE_IGNORED_DELTA = 0.01;
 int RUN = 0;
 
 // SETUP BLOCK
+int DataPin = 13;
+int ppmrange = 5000;
+unsigned long pwmtime;
 
+int PPM = 0;
+float pulsepercent=0;
 // DHT setup
 void setupDht() {
   Serial.println("DHT startup!");
@@ -107,27 +101,13 @@ void reandAndSendSensorsData() {
   Serial.println("Sending data from sensors");
 }
 
-bool isRedLEDCondition(float humidity, float temperature, int val) {
-  bool isHumidityBad = (humidity < 30 || humidity > 70);
-  bool isTemperatureBad = (temperature < 18 || temperature > 27);
-  bool isValBadCondition = (val > 1400);
-  return isHumidityBad || isTemperatureBad || isValBadCondition;
-}
-
-bool isGreenLEDCondition(float humidity, float temperature, int val) {
-  bool isHumidityGood = (humidity >= 30 && humidity <= 40) || (humidity >= 50 && humidity <= 70);
-  bool isTemperatureGood = (temperature >= 18 && temperature <= 21) || (temperature >= 24 && temperature <= 26);
-  bool isValGoodCondition = (val >= 800 && val <= 1400);
-  return isHumidityGood || isTemperatureGood || isValGoodCondition;
-}
-
-void setLEDColors(float humidity, float temperature, int val) {
-  if (isGreenLEDCondition(humidity, temperature, val)) {
+void setLEDColors(int val) {
+  if (val < 1300 && val > 800) {
     // Green LED condition is met
     digitalWrite(PIN_GREEN, HIGH);
     digitalWrite(PIN_RED, LOW);
     digitalWrite(PIN_BLUE, LOW);
-  } else if (isRedLEDCondition(humidity, temperature, val)) {
+  } else if (val > 1300) {
     // Red LED condition is met
     digitalWrite(PIN_GREEN, LOW);
     digitalWrite(PIN_RED, HIGH);
@@ -136,36 +116,29 @@ void setLEDColors(float humidity, float temperature, int val) {
     // All values are good, turn on green LED
     digitalWrite(PIN_GREEN, LOW);
     digitalWrite(PIN_RED, LOW);
-    digitalWrite(PIN_BLUE, LOW);
+    digitalWrite(PIN_BLUE, HIGH);
   }
 }
 
-void displayDataOnScreen() {
-  u8g2.clearBuffer();          // Clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr); // Choose a suitable font
-  u8g2.setCursor(2, 10);
-  u8g2.print("PPM: ");
-  u8g2.print(val);
+/*void displayDataOnScreen() {
 
-  u8g2.setCursor(2, 30);
-  u8g2.print("Temperature: ");
-  u8g2.print(DHT_TEMPERATURE);
-  u8g2.print(" *C");
+  pwmtime = pulseIn(DataPin, HIGH, 2000000) / 1000;
+  float pulsepercent = pwmtime / 1004.0;
+  PPM = ppmrange * pulsepercent;
 
-  u8g2.setCursor(2, 50);
-  u8g2.print("Humidity: ");
-  u8g2.print(DHT_HUMIDITY);
-  u8g2.print(" %");
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
 
-  u8g2.sendBuffer();          // Transfer internal memory to the display
+  
 }
+*/
 
 void setup()
 {
+
   Serial.begin(115200);
   Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASS);
 
-  pinMode(sensorPin, INPUT);
   pinMode(PIN_RED, OUTPUT);
   pinMode(PIN_GREEN, OUTPUT);
   pinMode(PIN_BLUE, OUTPUT);
@@ -173,23 +146,30 @@ void setup()
   // Set up timer to run every 5 sec
   timer.setInterval(5000L, reandAndSendSensorsData);
 
-  u8g2.begin();
+  pinMode(DataPin, INPUT);
 
+  delay(2000);
 }
+
 
 void loop() {
 
-  setLEDColors(DHT_HUMIDITY, DHT_TEMPERATURE, val);
+  pwmtime = pulseIn(DataPin, HIGH, 2000000) / 1000;
+  float pulsepercent = pwmtime / 1004.0;
+  PPM = ppmrange * pulsepercent;
+  Serial.print("CO2 Konzentration in der Luft in PPM: ");
+  Serial.println(PPM);
 
-  val = analogRead(32)*2;
-  Serial.print("raw = ");
-  Serial.println(val);
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
 
-  Blynk.virtualWrite(MQ135_BLYNK_VPIN_PPM, val);
+  setLEDColors(PPM);
+  Serial.println(DHT_HUMIDITY);
+  Serial.println(DHT_TEMPERATURE);
 
-  displayDataOnScreen();
+  Blynk.virtualWrite(MHZ_BLYNK_VPIN_PPM, PPM);
 
-  delay(2000);
+  delay(20);
 
   Blynk.run();  
   timer.run();
